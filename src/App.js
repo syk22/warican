e14ba5d50ccba0d567d65158aa80e3ca2356abca
Kaisei-Suzuki1990 <a51b.huk.sk@gmail.com>
2020-12-15 22:32:53 +0900

    add GroupList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import GroupList from "./components/GroupList.jsx";
 
 function App() {
+  const [ list, setList ] = useState([]);
+  const nameList = ["sayaka", "misho", "kaisei"];
+
+  useEffect(() =>{
+    console.log("useEffect!")
+    setList(prevArr =>[...prevArr, nameList])
+    console.log(list);
+  },[]);
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+     <GroupList list={list} />
     </div>
   );
 }

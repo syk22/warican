3af2d05f95fd51a0191baaad8b257119197a68aa
Naoto <nstm.mrl1119@gmail.com>
2020-12-17 05:54:27 -0800

    heroku push 3

--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Success from "./components/Success.jsx";
 import Canceled from "./components/Canceled.jsx";
 
 function Home() {
   return (
-    <Router>
       <Switch>
         <Route path="/success" component={Success} />
         <Route path="/canceled" component={Canceled} />
-        <Route path="/" />
+        <Route path="/" component={App}/>
       </Switch>
-    </Router>
   );
 }
+
 
 ReactDOM.render(
   <BrowserRouter>

e84cafeb34734f3c4254a25dbcde57d7f7c94df3
Naoto <nstm.mrl1119@gmail.com>
2020-12-16 18:36:24 -0800

    added hard link

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function Home() {
         <Route path="/canceled.html">
           <Canceled />
         </Route>
-        <Route path="/">
+        <Route path="https://warican.herokuapp.com/">
           <App />
         </Route>
       </Switch>
